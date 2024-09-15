885ce1113ce4f95ef2d584db0a98608ecc5677df
bhanupratap9013 <43758767+bhanupratap9013@users.noreply.github.com>
2024-09-15 13:58:51 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.multiple-text', {
+    strings: ['ForntEndDev', 'BackEndDev', 'DevOps'], // Array of strings to type
+    typeSpeed: 50,  // Speed at which characters are typed
+    backSpeed: 25,   // Speed at which characters are erased
+    backDelay: 1000, // Delay before starting to erase
+    startDelay: 500, // Delay before starting typing
+    loop: true,      // Loop the animation
+    showCursor: true // Show the blinking cursor
+  });
+  
